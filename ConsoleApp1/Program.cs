9449f16a6194355207832d3819550233c2b8792f
Rodrigo Groener <rg@objektkultur.de>
2020-09-14 18:43:47 +0200

    updated donet core example

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,41 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+
 using Microsoft.Management.Infrastructure;
-using Microsoft.Management.Infrastructure.Options;
 
 namespace ConsoleApp1
 {
     class Program
     {
-
-        /*
-         * "function Decode {If ($args[0] -is [System.Array]) {[System.Text.Encoding]::ASCII.GetString($args[0])}
-         * Else {\"Not Found\"}}
-         *
-         *
-         * $Monitors = Get-CimInstance WmiMonitorID -Namespace root\\wmi;
-         * ForEach ($Monitor in $monitors) {$Serial = Decode $Monitor.SerialNumberID -notmatch 0; if($Serial -ne 0){return $Serial}}");
-         */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            string Namespace = @"root\wmi";
-            string className = "WmiMonitorID";
+            PrintMonitorInfos();
+        }
 
-            CimInstance myDrive = new CimInstance(className, Namespace);
-
-            //myDrive.CimInstanceProperties.Add(CimProperty.Create("SerialNumberID", null, CimFlags.Key)); //CimProperty.Create("SerialNumberID", "C:", CimFlags.Key));
-            
+        private static void PrintMonitorInfos()
+        {
             CimSession mySession = CimSession.Create(null);
-            
-            CimInstance searchInstance = mySession.GetInstance(Namespace, myDrive);
-
 
             IEnumerable<CimInstance> queryInstances =
                 mySession.QueryInstances(@"root\wmi",
                     "WQL",
                     @"select * from WmiMonitorID");
+
+            foreach (var queryInstance in queryInstances)
+            {
+                foreach (var cimInstanceProperty in queryInstance.CimInstanceProperties)
+                {
+                    var value = cimInstanceProperty.CimType == CimType.UInt16Array
+                        ? ConvertUInt16ArrayToString(cimInstanceProperty)
+                        : Convert.ToString(cimInstanceProperty.Value);
+
+                    Console.WriteLine(cimInstanceProperty.Name + " " + value);
+                }
+            }
+        }
+
+        private static string ConvertUInt16ArrayToString(CimProperty cimInstanceProperty)
+        {
+            var value = cimInstanceProperty.Value;
+            if (value == null)
+            {
+                return String.Empty;
+            }
+            var ushortValues = (ushort[])value;
+
+            var listOfChars = ushortValues.Select(x => Char.ConvertFromUtf32(Convert.ToInt32(x)));
+
+            return string.Join("", listOfChars);
         }
     }
 }
